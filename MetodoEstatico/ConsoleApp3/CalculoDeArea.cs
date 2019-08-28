7e1a082a747bb5404306eb9358323fc1900bb2c1
alumno <alumno@utnfra.com.ar>
2019-08-27 21:45:55 -0300

    21:45

--- MetodoEstatico/ConsoleApp3/CalculoDeArea.cs
+++ MetodoEstatico/ConsoleApp3/CalculoDeArea.cs
@@ -6,8 +6,32 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp3
 {
-    class CalculoDeArea
+    public static class CalculoDeArea
     {
-        public static double float ()
+        public static float CalculoCuadrado (float altura, float largo )
+        {
+            float area = 0;
+            if(altura > 0 && largo > 0 )
+            {
+                area = altura * largo;
+            }
+            return area;
+        }
+
+        public static float CalculoTriangulo(float altura, float largo)
+        {
+            float area = CalculoDeArea.CalculoCuadrado(altura, largo) / 2;
+            return area;
+        }
+
+        public static float CalculoCirculo (float radio)
+        {
+            float area;
+            float pi =Convert.ToSingle(Math.PI);
+
+
+            area = radio * Convert.ToSingle(Math.Pow(pi, 2));
+            return area;
+        }
     }
 }
--- MetodoEstatico/ConsoleApp3/Ejercicio14.cs
+++ MetodoEstatico/ConsoleApp3/Ejercicio14.cs
@@ -10,6 +10,56 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+            string input;
+            float altura;
+            float largo;
+            float radio;
+            float area;
+            bool flag = false;
+
+            do
+            {
+                Console.ReadLine();
+                Console.WriteLine("Ingrese una altura: ");
+                input = Console.ReadLine();
+                if(float.TryParse(input, out altura) && altura > 0)
+                {
+                    flag = true;
+                }
+            } while (!flag);
+            
+            do
+            {
+                Console.ReadLine();
+                Console.WriteLine("Ingrese un largo: ");
+                input = Console.ReadLine();
+                if (float.TryParse(input, out largo) && largo > 0)
+                {
+                    flag = true;
+                }
+            } while (!flag);
+
+            do
+            {
+                Console.ReadLine();
+                Console.WriteLine("Ingrese un radio: ");
+                input = Console.ReadLine();
+                if (float.TryParse(input, out radio) && radio > 0)
+                {
+                    flag = true;
+                }
+            } while (!flag);
+
+            area = CalculoDeArea.CalculoCuadrado(altura, largo);
+            Console.WriteLine("El area de su cuadrado es de: {0}", area);
+
+            area = CalculoDeArea.CalculoTriangulo(altura, largo);
+            Console.WriteLine("El area de su triangulo es de: {0}", area);
+
+            area = CalculoDeArea.CalculoCirculo(radio);
+            Console.WriteLine("El area de su circulo es de: {0}", area);
+
+            Console.ReadLine();
         }
     }
 }
