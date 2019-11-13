da23a487964496407e2782c468bdb40c1bee9178
alumno <alumno@utnfra.com.ar>
2019-11-12 21:58:41 -0300

    22:00

--- ModeloSegundoParcial/20181122 - SP Lab II/20181122-SP/Alumno/Archivos/IArchivo.cs
+++ ModeloSegundoParcial/20181122 - SP Lab II/20181122-SP/Alumno/Archivos/IArchivo.cs
@@ -7,7 +7,7 @@ using Entidades;
 
 namespace Archivos
 {
-  interface IArchivo<T> //where T : Queue<Patente>
+  interface IArchivo<T> 
   {
     void Guardar(string archivo, T datos);
     void Leer(string archivo, out T datos);
